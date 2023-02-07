1a2b7bd8c8c21d5403b24a6686eb7cadbe559c86
Ivan <shuvalol.vana@gmail.com>
2023-02-07 12:02:52 +0200

    task50 done

--- task50/Program.cs
+++ task50/Program.cs
@@ -5,28 +5,28 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-int[,] a = new int [3,4];
-for (int i=0;i<a.GetLength(0);i++)
+int[,] a = new int[3, 4];
+for (int i = 0; i < a.GetLength(0); i++)
 {
-    for (int j=0;j<a.GetLength(1);j++)
+    for (int j = 0; j < a.GetLength(1); j++)
     {
-        a[i,j] = new Random().Next(0,10);
-        Console.Write($"{a[i,j]} ");
+        a[i, j] = new Random().Next(0, 10);
+        Console.Write($"{a[i, j]} ");
     }
-Console.WriteLine();
+    Console.WriteLine();
 }
 Console.WriteLine("Введите число, которое хотите найти:");
 int b = int.Parse(Console.ReadLine());
-int sum=0;
-for (int i=0;i<a.GetLength(0);i++)
+int sum = 0;
+for (int i = 0; i < a.GetLength(0); i++)
 {
-    for (int j=0;j<a.GetLength(1);j++)
+    for (int j = 0; j < a.GetLength(1); j++)
     {
-        if (a[i,j]==b)
+        if (a[i, j] == b)
         {
             Console.WriteLine($"{i},{j}");
-            sum+=1;
+            sum += 1;
         }
     }
 }
-if (sum==0){Console.WriteLine("Такого числа в массиве нет");}
+if (sum == 0) { Console.WriteLine("Такого числа в массиве нет"); }
